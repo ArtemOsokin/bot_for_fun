fc78518cccaacb9b97f7160b5b1579995ba633a5
Osokin.Art <aptem21@mail.ru>
2023-04-05 19:23:44 +0400

    change bot token env var

--- bot/misc/conf.py
+++ bot/misc/conf.py
@@ -5,7 +5,7 @@ from typing import Final
 class Settings:
     DEBUG: bool = environ.get('DEBUG', True)
 
-    TOKEN: Final = environ.get('BOT_TOKEN', 'define me!')
+    TOKEN: Final = environ.get('BOT', 'define me!')
 
     OPENAPI_KEY: Final = environ.get('OPENAPI_KEY', 'Need openapi_key!')
     OPENAPI_ORG: Final = environ.get('OPENAPI_ORG', 'Need openapi_organisation!')
