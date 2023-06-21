2921a5779a0fe6383da8f6f5e74227bb22039a2f
Osokin.Art <aptem21@mail.ru>
2023-06-21 10:15:52 +0300

    update check_users handler

--- bot/handlers/admin/users.py
+++ bot/handlers/admin/users.py
@@ -7,6 +7,9 @@ async def check_users(msg: Message):
     admin_db_service = AdminDBService(msg)
     users = await admin_db_service.get_users()
     answer = '\n'.join(
-        [f'{user.tg_id}: {user.username} {user.first_name} {user.last_name}' for user in users]
+        [
+            f'{user.tg_id}: {user.username} {user.first_name} {user.last_name} - {user.gpt_count_requests} GPT запросов'
+            for user in users
+        ]
     )
     await msg.answer(answer)
